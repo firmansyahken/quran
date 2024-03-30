cf3f56b1e0decf64e79be08353742fdcf70d9bad
firmansyahken <firmansyahken4@gmail.com>
2024-03-30 22:52:45 +0700

    Add Page Surah

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Router from "./utils/RouterHandler.js"
 import { Home } from "./pages/Home.js"
+import { Surah } from "./pages/Surah.js"
 
 const routes = {
   "/": {
     title: "Home",
     element: Home
+  },
+  "surah/:id": {
+    title: "Surah",
+    element: Surah
   }
 }
 
